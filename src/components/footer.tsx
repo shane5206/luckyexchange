2c6efdf8a821c17293ce768d266a758cd7205146
shane <s27119597@gmail.com>
2025-02-18 10:35:02 +0800

    Changes to be committed: 	modified:   src/components/footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,19 @@ export function Footer() {
     <footer className="container mt-10 flex flex-col items-center justify-between gap-6 py-10 sm:flex-row">
       <p className="text-center text-sm">© 2024 Lucky Ed, Inc. All rights reserved.</p>
       <div className="flex items-center gap-5">
+        <a 
+          href="https://share.evernote.com/note/537085fd-7fff-a727-5e6c-3d6b0d083ee1" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="text-muted-foreground hover:text-foreground"
+        >
+          <svg viewBox="0 0 24 24" className="size-5">
+            <path
+              d="M7.5 18C4.46 18 2 15.54 2 12.5S4.46 7 7.5 7H18c2.21 0 4 1.79 4 4s-1.79 4-4 4H9.5C8.12 15 7 13.88 7 12.5S8.12 10 9.5 10H17v1.5H9.5c-.55 0-1 .45-1 1s.45 1 1 1H18c1.38 0 2.5-1.12 2.5-2.5S19.38 9.5 18 9.5H7.5c-2.21 0-4 1.79-4 4s1.79 4 4 4H17V19H7.5z"
+              fill="currentColor"
+            />
+          </svg>
+        </a>
         <a href="#" className="text-muted-foreground hover:text-foreground">
           <svg viewBox="0 0 438.549 438.549" className="size-5">
             <path
