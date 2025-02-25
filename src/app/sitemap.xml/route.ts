9ce2cb336c10cdcc096146866a00c45be17512b0
shane <s27119597@gmail.com>
2025-02-25 10:10:35 +0800

    Changes to be committed: 	deleted:    public/sitemap.xml 	new file:   src/app/sitemap.xml/route.ts

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+
+const baseUrl = 'https://luckysports.cloud'
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/about</loc>
+        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/contact</loc>
+        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/register</loc>
+        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+      </url>
+    </urlset>`
+}
+
+export async function GET() {
+  const sitemap = generateSiteMap()
+
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=59'
+    }
+  })
+} 
